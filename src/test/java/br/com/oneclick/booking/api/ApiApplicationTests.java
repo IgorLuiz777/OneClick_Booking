ec9315e8e4a03b4f59b63e1a7c95a70fc5f9b847
IgorLuiz777 <igorluizpereiralima@gmail.com>
2024-09-19 10:54:14 -0300

    building project

---
+++ src/test/java/br/com/oneclick/booking/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.oneclick.booking.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
